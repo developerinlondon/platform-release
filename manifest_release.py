8c55eeb1cd325b2f7de94dc9e420074753477dff
Nayeem Syed <developerinlondon@gmail.com>
2018-12-06 17:40:54 +0000

    CPS-842 separate apply and build and add undo option (pull request #4)
    Approved-by: Aurimas Mickevicius <aurimasm@motabilityoperations.co.uk>
    Approved-by: Ioannis Georgiou <ioannisg@motabilityoperations.co.uk>

--- manifest_release.py
+++ manifest_release.py
@@ -13,15 +13,16 @@ parser = argparse.ArgumentParser(description='Options')
 parser.add_argument('--manifest-release-type', dest='manifest_release_type', default='minor', action='store', help="Manifest Release Type. major/minor/hotfix")
 parser.add_argument('--repo-release-type', default='minor', dest='repo_release_type', action='store', help="Type of release for repos - major/minor/hotfix")
 parser.add_argument('--skip-release-note', default=False, dest='skip_release_note', action='store_true', help="Skip Publishing a release_note.txt file")
+parser.add_argument('--build', default=False, dest='build', action='store_true', help="Build Mode. Builds the releases locally.")
 parser.add_argument('--apply', default=False, dest='apply', action='store_true', help="Apply Mode. Pushes the changes to remote.")
+parser.add_argument('--undo', default=False, dest='undo', action='store_true', help="Undo Mode. Updates remote by changing the tags that were last pushed.")
 parser.add_argument('--release-note-file', default='manifest_release_note.txt', dest='release_note_file', action='store', help="name of the release_note file")
 parser.add_argument('--verbose', default=False, dest='verbose', action='store_true', help="verbose Mode. Spits out more info if in verbose mode")
-parser.add_argument('--manifest-tags', default='openshift-cluster', dest='manifest_tags', action='store', help="Name of the Manifest Group to release.")
+parser.add_argument('--manifest-tags', default='build', dest='manifest_tags', action='store', help="Name of the Manifest Group to release.")
 parser.add_argument('--manifest-file', default='manifest.xml', dest='manifest_file', action='store', help="Name of the Manifest File to use.")
 parser.add_argument('--manifest-repo', default='cp_manifest', dest='manifest_repo', action='store', help="Name of the Manifest Repo to use.")
-parser.add_argument('--manifest-branch', default='master', dest='manifest_branch', action='store', help="Name of the Manifest Branch to make the release off of.")
+parser.add_argument('--manifest-version', default='master', dest='manifest_version', action='store', help="Name of the Manifest Branch or Tag to make the release off of.")
 args = parser.parse_args()
-
 
 ##### LOGIC ######
 # clone cp_manifest.xml repo master branch
@@ -47,89 +48,91 @@ release_note_source_file = workspace + '/release.txt'
 release_note_destination_file = workspace +'/'+ args.manifest_repo + '/release.txt'
 outputfile = workspace + '/output.xml'
 
+if args.build:
+  os.system( 'rm -fr '+workspace+'; mkdir -p '+workspace)
+  os.chdir(workspace)
+  os.system('git clone git@bitbucket.org:motabilityoperations/'+args.manifest_repo+'.git; cd '+args.manifest_repo+'; git checkout '+args.manifest_version+';')
+  os.chdir(workspace+'/'+args.manifest_repo)
 
-os.system( 'rm -fr '+workspace+'; mkdir -p '+workspace)
-os.chdir(workspace)
-os.system('git clone git@bitbucket.org:motabilityoperations/'+args.manifest_repo+'.git; cd '+args.manifest_repo+'; git checkout master;')
+  manifest_file_handler = open(outputfile,'w')
+  release_note_file_handler = open(release_note_source_file,'w')
 
-manifest_file_handler = open(outputfile,'w')
-release_note_file_handler = open(release_note_source_file,'w')
+  last_manifest_tag = versioning_library.get_last_tag(args.manifest_repo)
+  new_manifest_tag = versioning_library.get_new_tag(last_manifest_tag,args.manifest_release_type)
+  release_note_file_handler.write(versioning_library.generate_release_note(args.manifest_repo, last_manifest_tag, new_manifest_tag, args.skip_release_note))
 
-last_release_branch = versioning_library.get_last_release_branch(args.manifest_repo)
-new_release_branch  = versioning_library.get_new_release_branch(last_release_branch,args.manifest_release_type)
+  for line in open(manifest_file,'r'):
+    output_line = line
+    m = re.search('<project name="(.*?)".*?revision="(.*?)".*?groups="(.*?'+args.manifest_tags+'.*?)"',line)
+    if hasattr(m,'group'):
+      project_name = m.group(1)
+      manifest_version = m.group(2)
 
-last_manifest_tag = versioning_library.get_last_tag(args.manifest_repo)
-new_manifest_tag = versioning_library.get_new_tag(last_manifest_tag,args.manifest_release_type)
-release_note_file_handler.write(versioning_library.generate_release_note(args.manifest_repo, last_manifest_tag, new_manifest_tag, args.skip_release_note))
+      #### tag repo
+      os.system(
+        'cd '+workspace+'; rm -fr '+project_name+';'
+        +'git clone git@bitbucket.org:motabilityoperations/'+project_name+'.git; cd '+project_name+';'
+        +'git checkout master'
+        )
 
-for line in open(manifest_file,'r'):
-  output_line = line
-  m = re.search('<project name="(.*?)".*?revision="(.*?)".*?groups="(.*?'+args.manifest_tags+'.*?)"',line)
-  if hasattr(m,'group'):
-    project_name = m.group(1)
-    manifest_version = m.group(2)
+      os.chdir(workspace+'/'+project_name)
 
-    #### tag repo
-    os.system(
-      'cd '+workspace+'; rm -fr '+project_name+';'
-      +'git clone git@bitbucket.org:motabilityoperations/'+project_name+'.git; cd '+project_name+';'
-      +'git checkout master'
-      )
-
-    os.chdir(workspace+'/'+project_name)
-
-    head_tag=commands.getstatusoutput("git describe --contains `git rev-parse HEAD`")
-    last_version = versioning_library.get_last_tag(project_name)
-    if args.verbose:
-      print 'head_tag:'
-      print head_tag
-    if head_tag[0] == 0:
-      print "==> The latest commit on master already tagged to "+head_tag[1]+". Skipping!"
-      new_version = last_version
-    else:
-      new_version  = versioning_library.get_new_tag(last_version,args.repo_release_type)
-
+      head_tag=commands.getstatusoutput("git describe --contains `git rev-parse HEAD`")
+      last_version = versioning_library.get_last_tag(project_name)
       if args.verbose:
-        print 'new version:'
-        print new_version
-
-      os.system('git tag '+new_version)
-      print "==> Tagged with "+new_version
-      if args.apply:
-        os.system('git push --tags')
-        print "==> Pushed new tag "+new_version
+        print 'head_tag:'
+        print head_tag
+      if head_tag[0] == 0:
+        print "==> The latest commit on master already tagged to "+head_tag[1]+". Skipping!"
+        new_version = last_version
       else:
-        print "==> Skipped Pushing as '--apply' not passed!"
+        new_version  = versioning_library.get_new_tag(last_version,args.repo_release_type)
 
-      release_note_file_handler.write(versioning_library.generate_release_note(project_name, last_version, new_version, args.skip_release_note))
+        if args.verbose:
+          print 'new version:'
+          print new_version
 
-    #### End tag_repo
+        os.system('git tag '+new_version)
+        print "==> Tagged with "+new_version
 
-    # now update manifest
-    if manifest_version == 'refs/tags/'+new_version:
-      print project_name+": "+"skipping - "+manifest_version+ " to "+new_version
-    else:
-      print project_name+": "+"updating - "+manifest_version+ " to "+new_version
-      output_line = re.sub(r"revision=\".*?\"","revision=\"refs/tags/"+new_version+"\"",line)
-  manifest_file_handler.write(output_line)
+        release_note_file_handler.write(versioning_library.generate_release_note(project_name, last_version, new_version, args.skip_release_note))
 
-manifest_file_handler.close()
-release_note_file_handler.close()
+      #### End tag_repo
 
-os.chdir(workspace+'/'+args.manifest_repo)
-if new_release_branch == last_release_branch:
-  os.system('git checkout release/'+new_release_branch)
-else:
-  os.system('git checkout -b release/'+new_release_branch)
-os.system('cp '+release_note_source_file+' '+release_note_destination_file)
-os.system('cp '+outputfile+' '+manifest_file)
-os.system('git add -A')
-os.system('git commit -m"release '+new_manifest_tag+' created"')
-os.system('git tag '+new_manifest_tag)
+      # now update manifest
+      if manifest_version == 'refs/tags/'+new_version:
+        print project_name+": "+"skipping - "+manifest_version+ " to "+new_version
+      else:
+        print project_name+": "+"updating - "+manifest_version+ " to "+new_version
+        output_line = re.sub(r"revision=\".*?\"","revision=\"refs/tags/"+new_version+"\"",line)
+
+    manifest_file_handler.write(output_line)
+
+  manifest_file_handler.close()
+  release_note_file_handler.close()
+
+  os.chdir(workspace+'/'+args.manifest_repo)
+
+  os.system('mv '+release_note_source_file+' '+release_note_destination_file)
+  os.system('mv '+outputfile+' '+manifest_file)
+  os.system('git add -A')
+  os.system('git commit -m"release '+new_manifest_tag+' created"')
+  os.system('git tag '+new_manifest_tag)
+
 if args.apply:
-  print '==> Pushing Tags '+new_manifest_tag+' and Releases '+new_release_branch+'!'
-  os.system('git push --tags')
-  os.system('git push origin release/'+new_release_branch)
-else:
-  print '==> Skipping Pushing Tags '+new_manifest_tag+' and Releases '+new_release_branch+' as apply not passed!'
+  folder = os.listdir(workspace)
+  for project in folder:
+    if os.path.isdir(workspace + '/' + project):
+      print "==> Applying " + project
+      os.chdir(workspace + '/' + project)
+      os.system('git push --tags')
 
+if args.undo:
+  folder = os.listdir(workspace)
+  for project in folder:
+    if os.path.isdir(workspace + '/' + project):
+      tag = versioning_library.get_last_tag(project)
+      if tag != '0.0.0':
+        os.chdir(workspace+'/'+project)
+        print "==> Undoing " + project + " tag "+tag + "----> " + 'git push origin --tags :'+tag
+        os.system('git push origin --tags :'+tag)
--- versioning_library.py
+++ versioning_library.py
@@ -11,31 +11,6 @@ def get_last_tag(repo_name):
   else:
     return version[1]
 
-# get all the branches starting with the word 'release' to find out what the last release branch was then create a new release branch
-def get_last_release_branch(repo_name):
-    version = commands.getstatusoutput('git ls-remote --heads git@bitbucket.org:motabilityoperations/'+repo_name+' 2>/dev/null  | cut -f3,4 -d"/" | grep release | tail -1 | cut -f2 -d"/"')
-    if version[1] == '':
-      return '0.0.0'
-    else:
-      return version[1]
-
-def get_new_release_branch(last_version,release_type='minor'):
-    version_bits=last_version.split('.')
-    # bump version
-    if release_type == 'major':
-      version_bits[0] = str(int(version_bits[0])+1)
-      version_bits[1] = '0'
-      version_bits[2] = '0'
-    elif release_type == 'minor':
-      version_bits[1] = str(int(version_bits[1])+1)
-      version_bits[2] = '0'
-    elif release_type == 'hotfix':
-      print "hotfix does not change branch, so will work on: "+last_version
-
-    new_version = '.'.join(version_bits)
-    print "new version is: "+new_version
-    return new_version
-
 def insert_to_file(originalfile,string):
     print "==> updating release note on "+originalfile
     os.system('touch '+originalfile)
@@ -83,8 +58,6 @@ def generate_release_note(repo_name, last_version, new_version, empty_release_no
     release_note = release_note+commands.getstatusoutput("git log "+log_filter+' --pretty=format:%s')[1] + "\n\n"
 
     return release_note
-    # insert_to_file(release_note_file,release_note)
-    # print "==> Release Note added to "+release_note_file
 
 def update_tag(filename, repo, new_tag, outputfile):
   f = open(outputfile,'w')
